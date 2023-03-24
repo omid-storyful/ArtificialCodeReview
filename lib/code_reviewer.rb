cd18900cf06121e13b71643f1add2ceb5c5356ad
omid-storyful <omid.bachari@storyful.com>
2023-03-24 15:15:43 +0000

    [feature] Add new review feature and make some other changes

--- lib/code_reviewer.rb
+++ lib/code_reviewer.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 require_relative './open_ai_adapter'
-
 # Responsible for providing class methods that return desired code review
 class CodeReviewer
   class << self
-    def explain(diff:)
+    def analyze(diff:, action:)
+      handle_chat(diff: diff, system_prompt: Prompt.const_get("#{action}_CHANGES_SYSTEM_ROLE"),
+                  user_prompt: Prompt.const_get("#{action}_CHANGES"))
+    end
+
+    private
+
+    def handle_chat(diff:, system_prompt:, user_prompt:)
       messages = [
-        { role: 'system', content: Prompt::EXPLAIN_CHANGES_SYSTEM_ROLE },
-        { role: 'user', content: Prompt::EXPLAIN_CHANGES + diff }
+        { role: 'system', content: system_prompt },
+        { role: 'user', content: user_prompt + diff }
       ]
 
       open_ai_conversation = OpenAiAdapter.new(input: messages)
